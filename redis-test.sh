e1f21108d4a22716360d10daaa54d221a5f30440
dizarate <diegoazd@hotmail.com>
2017-02-03 20:16:41 -0600

    add test sh

---
+++ redis-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
+curl -XPOST 'http://localhost:8080/redis-test/users/1' &
